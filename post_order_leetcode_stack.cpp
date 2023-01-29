d49d35a3965dc2fef3660dc02a065b91ff744f15
Vishal Gupta <114002191+vishalxgithub@users.noreply.github.com>
2023-01-29 11:23:34 +0530

    Create post_order_leetcode_stack.cpp

---
+++ post_order_leetcode_stack.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        stack<TreeNode*> s1;
+        stack<TreeNode*> s2;
+        s1.push(root);
+        while(!s1.empty()){
+            TreeNode* temp = s1.top();
+            s1.pop();
+            s2.push(temp);
+
+            if(temp->left)
+                s1.push(temp->left);
+            if(temp->right)
+                s1.push(temp->right);
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return ans;
+    }
+};
